20ff21ca5556c63ec5b4e4fbfe5a998040ee2dbe
maya-fakih <alfakihmaya1@gmail.com>
2025-12-19 10:56:54 +0200

    ordering

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,3 @@
-# sudoku.py
-
 from collections import deque
 import copy
 
@@ -8,7 +6,6 @@ class CSP_Sudoku:
     def __init__(self, board):
         self.n = 9
         self.board = copy.deepcopy(board)
-
         self.domain = [1,2,3,4,5,6,7,8,9]
 
         self.AvailableAssignments = []
@@ -20,12 +17,22 @@ class CSP_Sudoku:
                 else:
                     self.AvailableAssignments[i].append(deque())
 
+    # =========================
+    # MRV heuristic
+    # =========================
     def getUnassignedVariable(self):
+        min_len = 10
+        pos = (-1, -1)
+
         for i in range(self.n):
             for j in range(self.n):
                 if self.board[i][j] == -1:
-                    return i, j
-        return -1, -1
+                    d = len(self.AvailableAssignments[i][j])
+                    if d < min_len:
+                        min_len = d
+                        pos = (i, j)
+
+        return pos
 
     def isGoal(self):
         for i in range(self.n):
@@ -58,7 +65,6 @@ class CSP_Sudoku:
     def forward_check(self, row, col, value):
         removed = []
 
-        # Row & Column
         for k in range(self.n):
             if self.board[row][k] == -1 and value in self.AvailableAssignments[row][k]:
                 self.AvailableAssignments[row][k].remove(value)
@@ -72,7 +78,6 @@ class CSP_Sudoku:
                 if not self.AvailableAssignments[k][col]:
                     return False, removed
 
-        # Box
         box_row = (row // 3) * 3
         box_col = (col // 3) * 3
         for i in range(box_row, box_row + 3):
@@ -89,11 +94,51 @@ class CSP_Sudoku:
         for r, c, v in removed:
             self.AvailableAssignments[r][c].append(v)
 
+    # =========================
+    # Correct Singleton Propagation
+    # =========================
+    def propagate_singletons(self):
+        assigned = []
+        removed_all = []
+
+        progress = True
+        while progress:
+            progress = False
+            for i in range(self.n):
+                for j in range(self.n):
+                    if self.board[i][j] == -1 and len(self.AvailableAssignments[i][j]) == 1:
+                        value = self.AvailableAssignments[i][j][0]
+
+                        self.board[i][j] = value
+                        assigned.append((i, j))
+
+                        ok, removed = self.forward_check(i, j, value)
+                        removed_all.extend(removed)
+
+                        if not ok:
+                            return False, assigned, removed_all
+
+                        progress = True
+
+        return True, assigned, removed_all
+
+    # =========================
+    # Backtracking + FC + MRV + SP
+    # =========================
     def csp_cells(self):
         if self.isGoal():
             return True
 
+        ok, assigned, removed = self.propagate_singletons()
+        if not ok:
+            self.restore_domains(removed)
+            for r, c in assigned:
+                self.board[r][c] = -1
+            return False
+
         row, col = self.getUnassignedVariable()
+        if row == -1:
+            return self.isGoal()
 
         domain_snapshot = copy.deepcopy(self.AvailableAssignments[row][col])
 
@@ -103,15 +148,20 @@ class CSP_Sudoku:
             if self.isValidConstraints(row, col, value):
                 self.board[row][col] = value
 
-                ok, removed = self.forward_check(row, col, value)
-                if ok:
-                    if self.csp_cells():
-                        return True
+                ok, removed2 = self.forward_check(row, col, value)
+                if ok and self.csp_cells():
+                    return True
 
-                self.restore_domains(removed)
+                self.restore_domains(removed2)
                 self.board[row][col] = -1
 
         self.AvailableAssignments[row][col] = domain_snapshot
+
+        # rollback singleton propagation
+        for r, c in assigned:
+            self.board[r][c] = -1
+        self.restore_domains(removed)
+
         return False
 
     def solve(self):
